0b3302c1b395bc3debf7b226ccf8da3f2b665842
Zora109 <zhuoranshi109@gmail.com>
2025-11-03 10:34:24 -0600

    Update petrie_simulation.py

--- src/ica/19-classes/petrie_simulation.py
+++ src/ica/19-classes/petrie_simulation.py
@@ -19,8 +19,9 @@ class Employee:
         saves those values to instance variables. It also initializes the
         variable that holds the comments received by this employee to zero.
         """
-        # TODO: Implement this method then remove this line
-        pass
+        self.gender = gender                # "Man" or "Woman"
+        self.will_comment = bool(will_comment)  # True/False
+        self.comments_received = 0          # start at 0
 
     def __str__(self):
         """
@@ -37,8 +38,8 @@ def print_employee_list(lst):
     Given a list of employees, this method will print the details of each employee
     by using the print() method
     """
-    # TODO: Implement this function then remove this line
-    pass
+    for emp in lst:
+        print(emp)
 
 
 def create_employees(total_num):
@@ -46,8 +47,21 @@ def create_employees(total_num):
     Takes in the number of employees to make, builds and returns a list that contains
     that many employees. It ensures that ~80% are men and the rest women.
     """
-    # TODO: Implement this function then remove this line
-    pass
+    if total_num <= 0:
+        return []
+
+    num_men = round(total_num * 0.8)
+    num_women = total_num - num_men
+
+    employees = []
+    # default will_comment=False; create_commenters() will set 20% later
+    for _ in range(num_men):
+        employees.append(Employee("Man", False))
+    for _ in range(num_women):
+        employees.append(Employee("Woman", False))
+
+    random.shuffle(employees)  # randomize order a bit
+    return employees
 
 
 def create_commenters(lst):
@@ -55,8 +69,25 @@ def create_commenters(lst):
     Given a list of employees, make 20% of each gender be sexist employees. This
     method should not return anything.
     """
-    # TODO: Implement this function then remove this line
-    pass
+    # Split by gender
+    men = [e for e in lst if e.gender == "Man"]
+    women = [e for e in lst if e.gender == "Woman"]
+
+    # First, set everyone to not comment, then flip 20% to True
+    for e in lst:
+        e.will_comment = False
+
+    def activate_twenty_percent(group):
+        if not group:
+            return
+        k = max(1, math.floor(len(group) * 0.20)) if len(group) > 0 else 0
+        # If group is very small (e.g., 1–4), floor could be 0; ensure at least 1 if group exists
+        chosen = random.sample(group, k=min(k, len(group)))
+        for e in chosen:
+            e.will_comment = True
+
+    activate_twenty_percent(men)
+    activate_twenty_percent(women)
 
 
 def generate_comments(lst):
@@ -65,8 +96,23 @@ def generate_comments(lst):
     another employee of the opposite gender, chosen randomly. This method should
     not return anything
     """
-    # TODO: Implement this function then remove this line
-    pass
+    # Precompute targets by gender
+    men = [e for e in lst if e.gender == "Man"]
+    women = [e for e in lst if e.gender == "Woman"]
+
+    for e in lst:
+        if not e.will_comment:
+            continue
+        if e.gender == "Man":
+            targets = women
+        else:
+            targets = men
+
+        if not targets:
+            continue  # no opposite-gender targets available
+
+        target = random.choice(targets)
+        target.comments_received += 1
 
 
 def average_comments(lst):
@@ -74,8 +120,24 @@ def average_comments(lst):
     Returns a tuple that represents the average amount of comments received for men and women
     respectively. Return statement will be in the form (<avg_for_men>, <avg_for_women>)
     """
-    # TODO: Implement this function then remove this line
-    pass
+    men_comments = 0
+    men_count = 0
+    women_comments = 0
+    women_count = 0
+
+    for e in lst:
+        if e.gender == "Man":
+            men_comments += e.comments_received
+            men_count += 1
+        elif e.gender == "Woman":
+            women_comments += e.comments_received
+            women_count += 1
+
+    men_avg = men_comments / men_count if men_count else 0.0
+    women_avg = women_comments / women_count if women_count else 0.0
+
+  
+    return (round(men_avg, 2), round(women_avg, 2))
 
 
 def main():
